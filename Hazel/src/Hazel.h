caf8259a2e0b4530555f57c21cd4c7b2867abf9e
yanxihang <964343812@qq.com>
2022-05-03 01:43:29 +0800

    Setup basic Application and Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//For use by Hazel app
+
+#include "Hazel\Applcation.h"
+
+// --Entry Point---------------------
+#include "Hazel\EntryPoint.h"
+//-----------------------------------
---
+++ Hazel/src/Hazel/Applcation.cpp
@@ -0,0 +1,26 @@
+#include "Applcation.h"
+
+namespace Hazel
+{
+	Hazel::Application::Application()
+	{
+
+	}
+
+	Hazel::Application::~Application()
+	{
+
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+
+	Application* CreateApplication()
+	{
+		return nullptr;
+	}
+
+}
+
